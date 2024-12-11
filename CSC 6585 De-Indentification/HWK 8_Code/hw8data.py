5a5409cffee19adea7f05b88c9ac576d0acf4f1d
Blaine Swieder <bcswieder42@tntech.edu>
2024-12-11 14:59:49 -0600

    Removed embedded Git repository

---
+++ CSC 6585 De-Indentification/HWK 8_Code/hw8data.py
@@ -0,0 +1,26 @@
+from typing import List
+
+# Dataset; sensitive column is the last column.
+DATASET: List[List[str]] = [
+    # name, age, sex, city, state, country, icd10
+    ["Kim Deal", 63, "female", "Dayton", "Ohio", "USA", "J64"],
+    ["Gaye Advert", 68, "female", "Bideford", "Devon", "UK", "J60"],
+    ["Cait O’Riordan", 59, "female", "Lagos City", "Lagos", "Nigeria", "J62"],
+    ["Michele Temple", 65, "female", "Van Wert", "Ohio", "USA", "J63"],
+    ["Sara Lee", 69, "female", "", "West Midlands", "UK", "J62"],
+    ["Gail Greenwood", 64, "female", "Providence", "Rhode Island", "USA", "J61"],
+    ["Enid Williams", 64, "female", "City of London", "London", "UK", "J67"],
+    ["Abby Travis", 54, "female", "Los Angeles", "California", "USA", "J70"],
+    ["Juliana Hatfield", 57, "female", "Wiscasset", "Maine", "USA", "J68"],
+    ["Carol Kaye", 89, "female", "Everett", "Washington", "USA", "J62"],
+    ["William Earl Collins", 72, "male", "Cincinnatti", "Ohio", "USA", "J62"],
+    ["Larry Graham", 78, "male", "Beaumont", "Texas", "USA", "J63"],
+    ["Jaco Pastorius", 72, "male", "Norristown", "Pennsylvania", "USA", "J62"],
+    ["Ronald Carter", 87, "male", "Ferndale", "Michigan", "USA", "J67"],
+    ["Philip Lesh", 84, "male", "Berkeley", "California", "USA", "J62"],
+    ["Stanley Clarke", 73, "male", "Philadelphia", "Pennsylvania", "USA", "J62"],
+    ["John Paul Jones", 78, "male", "Sidcup", "Kent", "UK", "J63"],
+    ["Verdine White", 73, "male", "Chicago", "Illinois", "USA", "J62"],
+    ["Geezer Butler", 75, "male", "Birmingham", "West Midlands", "UK", "J62"],
+    ["Michael Peter Balzary", 62, "male", "Melbourne", "Victoria", "Australia", "J62"],
+]
